04c8dcb3aab15e9ee7f5812d4aec5fad433dcc20
moreloalex4-netizen <moreloalex4@gmail.com>
2026-09-13 20:18:20 -0500

    Remove image object from content.js
    Removed an image object from the array in content.js.

--- content.js
+++ content.js
@@ -36,7 +36,6 @@ const CONTENIDO = {
           {
             type: "image",
             src: "images/paisaje-cafetero.svg",
-            { type: "image", src: "images/mi-foto.jpg", caption: "Descripción de la foto" },
             caption: "Paisaje Cultural Cafetero — Patrimonio de la Humanidad UNESCO, 2011"
           },
           {
